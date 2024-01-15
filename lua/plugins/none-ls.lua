3950f01a386387f6a085bd55eb9d4f0d77a802ce
Ramonnn <29675067+Ramonnn@users.noreply.github.com>
2024-01-15 14:28:19 +0100

    fixed ansible error

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
 				null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.jsonlint,
-        null_ls.builtins.diagnostics.ansible-lint,
+        null_ls.builtins.diagnostics.ansiblelint,
 			},
 		})
 
